385e128439898f287b21f3007bb1ecc94b64bb5f
Hoventage <ilya.filimonov.88@bk.ru>
2021-04-11 04:33:57 +0300

    not first commit with JS

--- myFirstScripts/learnJavascriptBasics/learnJSbasics.js
+++ myFirstScripts/learnJavascriptBasics/learnJSbasics.js
@@ -11,7 +11,7 @@ variable = String(variable);
 console.log(typeof variable);*/
 
 //string to number
-console.log(typeof ("6"/"3"));
+/*console.log(typeof ("6"/"3"));
 let value = "iweyurtoipwyuerpoituwpeoirt";
 let numValue = Number(value);
 
@@ -19,14 +19,76 @@ if (isNaN(numValue)){
     console.log("This is what a'm talking about");
 } else {
     console.log("lol");
+}*/
+
+//Convert variables to numbers
+/*
+let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+
+alert(+a + +b);*/
+
+//strings comparison
+/*
+let firstLetter = "а";
+let lastLetter = "я";
+
+if (firstLetter < lastLetter) {
+    console.log(firstLetter);
+} else {
+    console.log(lastLetter);
 }
+*/
+
+//if statement
+/*
+let kokoko = "kokoko";
+(kokoko === "kokoko") ? alert("ok") : alert("bullshit!");*/
+
+/*let jsOfficialName = prompt("Какое \"официальное\" название JavaScript?", "");
+
+if (jsOfficialName === "ECMAScript") {
+    alert("Правильно!");
+} else {
+    alert("Не знаете? \"ECMAScript\"");
+}*/
+
+//let num = prompt("Введите число", "");
+
+/*f (num > 0) {
+    alert(1);
+} else if (num < 0) {
+    alert(-1);
+} else {
+    alert(0);
+}*/
+
+//(num > 0) ? alert(1) : num < 0 ? alert(-1) : alert(0);
+
+/*
+let result;
+let a = +prompt("Введите число", "");
+let b = +prompt("Введите число", "");
 
 
-true + false
-4 + 5 + "px"
-"4px" - 2
-7 / 0
-"  -9  " + 5
-null + 1
-undefined + 1
-" \t \n" - 2
+result = (a + b < 4) ? 'Мало' : 'Много';
+alert(result);*/
+
+/*let message;
+
+if (login == 'Сотрудник') {
+    message = 'Привет';
+} else if (login == 'Директор') {
+    message = 'Здравствуйте';
+} else if (login == '') {
+    message = 'Нет логина';
+} else {
+    message = '';
+}*/
+
+/*
+let username = prompt("Введите логин", "");
+let message;
+
+message = (username === 'Сотрудник') ? 'Привет' : (username === 'Директор') ? 'Здравствуйте' : (username === '') ? 'Нет логина' : '';
+alert(message);*/
